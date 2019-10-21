f0948375fa65053f8ef5ee1d08809d2ecc983a4e
Stoney Kang <sikang99@gmail.com>
2019-10-21 22:40:26 +0900

    update contents

---
+++ compose/example03/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	redis "gopkg.in/redis.v4"
+)
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/sikang99/docker-example
 
 go 1.12
+
+require (
+	gopkg.in/bsm/ratelimit.v1 v1.0.0-20160220154919-db14e161995a // indirect
+	gopkg.in/redis.v4 v4.2.4
+)
